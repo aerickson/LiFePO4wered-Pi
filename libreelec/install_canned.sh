47bb85766f9dc62892c31bc8c30d0c262a1dd24e
Andrew J. Erickson <aerickson@gmail.com>
2024-06-21 13:47:37 -0700

    add canned binaries for rpi4 and installation tool

---
+++ libreelec/install_canned.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+HOST=$1
+
+if [[ -z "$HOST" ]]; then
+  echo "FATAL: Please specify a host!"
+  exit 1
+fi
+
+# TODO: check that build dir exists
+
+ROOT_DIR="/storage/LiFePo4wered"
+ssh root@$HOST "mkdir -p $ROOT_DIR"
+
+scp autostart.sh root@$HOST:/storage/.config/autostart.sh
+scp canned/lifepo4wered-daemon root@$HOST:$ROOT_DIR/
+scp canned/lifepo4wered-cli root@$HOST:$ROOT_DIR/
+
+
+ssh root@$HOST "chmod 755 /storage/.config/autostart.sh"
+ssh root@$HOST "chmod 755 $ROOT_DIR/lifepo4wered*"
